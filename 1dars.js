50221e38b4eb247701c20040428f5323431429d3
janobsmile <janobsmile>
2022-01-06 15:26:35 +0500

    initial commit

---
+++ 1dars.js
@@ -0,0 +1,188 @@
+
+// 1-misol
+//let p, a = 12;
+
+//p = 4 * a * Math.PI;
+
+//console.log(p);
+
+
+//let x1 = 4, x2 = 1, y1 =4, y2 =0;
+
+//console.log(Math.pow((Math.pow((x2-x1), 2) + Math.pow((y2 - y1), 2)), 1/2));
+
+
+//1-misol
+//let p, a = 3;
+//p = 4 * a;
+//console.log(p);
+
+// 2-misol
+//let s, a = 4;
+//console.log(s = Math.pow( a, 2 ))
+
+// 3-misol
+//let s, p, a = 4, b = 5;
+//    s = a * b;
+//    p = 2 * (a + b);
+//console.log(s, p)
+
+// 4 - misol
+//let l, PI, d = 4;
+//l = Math.PI * d;
+//console.log(l);
+
+// 5 - misol
+// let v, s, a = 5;
+// v = Math.pow(5,3);
+// s = 6 * Math.pow(5,2);
+// console.log(v, s);
+
+// 6 - misol
+//let v, s, a = 4, b = 5, c = 4;
+//v = a * b * c;
+//s = 2 * (a * b + b * c + a * c);
+//console.log(v, s);
+
+// 7 - misol
+//let S, L, R = 4;
+//L = 2 * Math.PI * R;
+//S = Math.PI * Math.pow(R,2);
+//console.log(L, S);
+
+// 8 - misol
+//let S, a = 3, b = 4;
+//S = ( a + b) / 2
+//console.log(S)
+
+// 9 - misol
+//let S, a = 6, b = 7;
+//S = Math.sqrt(a * b);
+//console.log(S)
+
+// 10 - misol
+//let S, K, V, a = 4, b = 5;
+//S = a + b;
+//K = a * b;
+//V = Math.pow(4,2) + Math.pow(5,2)
+//console.log(S, V)
+
+ //11 - misol
+//let S, K, a = 6, b = 8;
+//S = a + b;
+//K = a * b;
+//console.log(S, K)
+
+// 12 - misol
+//let c, P, a = 2, b = 4;
+//c = Math.sqrt(Math.pow(2,2) + Math.pow(4,2));
+//P = a + b + c;
+//console.log(c, P);
+
+// 13 - misol
+//let R1 = 4, R2 = 2, S1, S2, S3, T;
+//R1 > R2;
+//S1 = Math.PI * R1;
+//S2 = Math.PI * R2;
+//S3 = Math.PI * (R1 - R2);
+//S3 = S1 - S2;
+//console.log(S3);
+
+// 14 - dars
+//let L = 4, S, R;
+//R = L / (2 * Math.PI)
+//S = Math.PI * Math.pow(R,2)
+//console.log(R, S);
+
+// 15 - misol
+//let S = 4, d, R, L;
+//R = Math.sqrt(S / Math.PI);
+//d = 2 * R;
+//L = 2 * Math.PI * R;
+//console.log( d, R);
+
+// 16 - misol
+//Modul
+// 17 - misol
+
+// 17 - misol
+//let S, AC, BC, A = 3, B = 4, C = 6;
+//AC = A + C;
+//BC = B + C;
+//S = A + B + C;
+//console.log(AC, BC, S);
+
+// 18 - misol
+//let S, AC, BC, A = 3, B = 4, C = 6;
+//AC = A + C;
+//BC = B + C;
+//S = AC * BC;
+//console.log(S);
+
+// 19 - misol
+//let P, S, a, b, x1 = 1, x2 = 5, y1 = 5, y2 = 1;
+//a = x2 - x1;
+//b = y2 - y1;
+//P = 2 * a + 2 * b;
+//S = a * b;
+//console.log(P, S);
+
+// 20 - misol
+//let L, x1 = 1, x2 = 5, y1 = 3, y2 = 4;
+//L = Math.sqrt((x2 - x1) + (y2 - y1));
+//console.log(L);
+
+// 21 - misol
+//let x1 = 4, x2 = 4, x3 = 6, y1 = 1, y2 = 3, y3 = 5, //S, p, a, b, c;
+//a = Math.sqrt((x2 - x1) + (y2 - y1));
+//b = Math.sqrt((x3 - x2) + (y3 - y2));
+//c = Math.sqrt((x3 - x1) + (y3 - y1));
+//p = (a + b + c) / 2;
+//S = Math.sqrt(p * (p - a) * (p - b) * (p - c));
+//console.log(S, p);
+
+// 22 - misol
+//let A = 2, B = 4;
+//console.log(A, B);
+
+// 23 - misol
+//let A =2, B = 4, C = 5;
+
+// 24 - misol
+
+// 25 - misol
+//let y, x = 2;
+//y = 3 * Math.pow(x,6) - 6 * Math.pow(x,2) - 7;
+//console.log(y);
+
+// 26 - misol
+//let y, x = 2;
+//y = 4 * Math.pow((x - 3),6) - 7 * (Math.pow((x - 3),3)) + 2;
+//console.log(y);
+
+// 27 - misol
+//let A = 4;
+//x1 = Math.pow(A,2);
+//x2= Math.pow(A,4);
+//x3 = Math.pow(A,8);
+//console.log(x1, x2, x3);
+
+// 28 - misol
+//let A = 2;
+//x1 = Math.pow(A,2);
+//x2= Math.pow(A,3);
+//x3 = Math.pow(A,5);
+//x4 = Math.pow(A,10);
+//x5= Math.pow(A,15);
+//console.log(x1, x2, x3, x4, x5);
+
+// 29 - misol
+//let R, a =  360;
+//R = a * Math.PI / 180;
+//(0 < a < 2 * Math.PI);
+//console.log(R);
+
+// 30 - misol
+//let a, R = 3 * Math.PI / 4;
+//a = R * 180 / Math.PI;
+//console.log(a);
